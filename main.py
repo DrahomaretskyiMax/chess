54a2bab1cd38a5a2d2a9e3228ecc97c4d80eeed5
DrahomaretskyiMax <max.drahomaretskyi@student.howest.be>
2026-02-28 18:52:40 +0100

    feat: implement bishop queen and king movement using board movements

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from board import Board
 
 board = Board()
 
-pawn = board.get_piece("a2")
-pawn.move()
+bishop = board.get_piece("c1")
+bishop.move("ForwardRight", 2)
+
+queen = board.get_piece("d1")
+queen.move("Forward", 2)
+
+king = board.get_piece("e1")
+king.move("Forward")
